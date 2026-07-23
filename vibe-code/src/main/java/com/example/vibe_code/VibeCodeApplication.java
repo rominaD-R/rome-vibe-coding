2b23cb30c3591a0041424dd0baf4683a0b4d56f9
Romina Diaz-Rivero <romina.diazrivero@gmail.com>
2026-07-22 19:38:27 -0500

    first commmit

---
+++ vibe-code/src/main/java/com/example/vibe_code/VibeCodeApplication.java
@@ -0,0 +1,15 @@
+package com.example.vibe_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeCodeApplication.class, args);
+
+		// hello
+	}
+
+}
